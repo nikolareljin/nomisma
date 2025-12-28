009e55fa7d15eb46583b8b7feb9f8442f012c383
Nikola Reljin <nikola.reljin@gmail.com>
2025-12-28 17:11:14 -0500

    chore: update testing API with bash (direct testing)

---
+++ testing/test_api.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Load environment variables
+source .env
+
+# Define the image to upload
+IMAGE_PATH="./testing/images/NOM-000_front.jpg"
+MIME_TYPE=$(file -b --mime-type "${IMAGE_PATH}")
+NUM_BYTES=$(wc -c < "${IMAGE_PATH}")
+DISPLAY_NAME=IMAGE
+GEMINI_MODEL="gemini-2.5-flash:generateContent" # Alternative: gemini-2.5-flash
+
+tmp_header_file=upload-header.tmp
+
+# Initial resumable request defining metadata.
+# The upload url is in the response headers dump them to a file.
+curl "https://generativelanguage.googleapis.com/upload/v1beta/files" \
+  -H "x-goog-api-key: $GEMINI_API_KEY" \
+  -D upload-header.tmp \
+  -H "X-Goog-Upload-Protocol: resumable" \
+  -H "X-Goog-Upload-Command: start" \
+  -H "X-Goog-Upload-Header-Content-Length: ${NUM_BYTES}" \
+  -H "X-Goog-Upload-Header-Content-Type: ${MIME_TYPE}" \
+  -H "Content-Type: application/json" \
+  -d "{'file': {'display_name': '${DISPLAY_NAME}'}}" 2> /dev/null
+
+upload_url=$(grep -i "x-goog-upload-url: " "${tmp_header_file}" | cut -d" " -f2 | tr -d "\r")
+rm "${tmp_header_file}"
+
+# Upload the actual bytes.
+curl "${upload_url}" \
+  -H "x-goog-api-key: $GEMINI_API_KEY" \
+  -H "Content-Length: ${NUM_BYTES}" \
+  -H "X-Goog-Upload-Offset: 0" \
+  -H "X-Goog-Upload-Command: upload, finalize" \
+  --data-binary "@${IMAGE_PATH}" 2> /dev/null > file_info.json
+
+file_uri=$(jq -r ".file.uri" file_info.json)
+echo file_uri=$file_uri
+
+# Now generate content using that file
+curl "https://generativelanguage.googleapis.com/v1beta/models/${GEMINI_MODEL}" \
+    -H "x-goog-api-key: $GEMINI_API_KEY" \
+    -H 'Content-Type: application/json' \
+    -X POST \
+    -d '{
+      "contents": [{
+        "parts":[
+          {"file_data":{"mime_type": "'"${MIME_TYPE}"'", "file_uri": "'"${file_uri}"'"}},
+          {"text": "Caption this image. Estimate value on the collectibles market for this coin. Compare to other similar coins and recommend the pricing."}]
+        }]
+      }' 2> /dev/null > response.json
+
+cat response.json
+echo
+
+jq ".candidates[].content.parts[].text" response.json
